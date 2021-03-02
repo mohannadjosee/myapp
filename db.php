eaacff118c71c90c4c17c6475c4f3e05818efd29
mohannadjosee <79788537+mohannadjosee@users.noreply.github.com>
2021-03-02 19:43:29 +0000

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$pdo = new PDO("sqlite:mydb.db");
+
+$st = $pdo->query("SELECT *FROM test;');
+
+$rows = $st->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<pre>';
+
+var_dump($rows);
+
+echo '</pre>';
+
+?>
